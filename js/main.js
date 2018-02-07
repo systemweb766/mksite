c6f6752a026573dbe8a1ea312939c39ac3e15e8c
regista766 <33421982+systemweb766@users.noreply.github.com>
2018-02-08 04:46:32 +0900

    merge commit 201802080446

--- js/main.js
+++ js/main.js
@@ -46,8 +46,10 @@ document.onreadystatechange = function() {
   if(document.readyState == "complete") {
     var hero = 'hero';
     var heroTxtBox = 'heroTxtBox';
+    var intro = 'intro';
     var target1 = document.getElementsByClassName(hero)[0];
     var target2 = document.getElementsByClassName(heroTxtBox)[0];
+    var target3 = document.getElementsByClassName(intro)[0];
     if(target1 && target2) {
       TweenMax.to('.' + hero, 1.5, {
         opacity: 1,
@@ -60,10 +62,16 @@ document.onreadystatechange = function() {
         })
       });
     }
-    else if(target1 && !target2) {
+    else if(target1 && !target2 && target3) {
       TweenMax.to('.' + hero, 1.5, {
         opacity: 1,
-        ease:Expo.easeInOut
+        ease:Expo.easeInOut,
+        onComplete: TweenMax.to('.' + intro, 2, {
+          opacity:1,
+          x:0,
+          delay:0.5,
+          ease:Power2.easeInOut
+        })
       });
     }
   }
