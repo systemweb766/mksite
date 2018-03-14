e1b192649a18bf0f968684f6c06bc68ebddebae5
systemweb766 <systemweb766@gmail.com>
2018-03-15 00:23:31 +0900

    update20180315

--- js/main.js
+++ js/main.js
@@ -1,47 +1,5 @@
 'use strict'
 
-
-// function loadScript(src, callback) {
-//   var done = false;
-//   var head = document.getElementsByTagName('head')[0];
-//   var script = document.createElement('script');
-//   script.src = src;
-//   head.appendChild(script);
-//
-//   //Attach handlers for all browsers
-//   script.onload = script.onreadystatechange = function(){
-//     if(!done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete")){
-//       callback();
-//       //Hande memory leak in IE
-//       script.onload = script.onreadystatechange = null;
-//       if(head && script.parentNode) {
-//         head.removeChild(script);
-//       }
-//     }
-//   };
-// };
-// loadScript('./../js/nav.js', function(){
-//   var id = 'navigation';
-//   var classOpen = 'is-Open';
-//   var classNone = 'is-none';
-//   var targetId = document.getElementById(id);
-//
-//
-//   var navi = new Navi(targetId, classOpen, classNone);
-//
-//   /* TODO:クリックイベントでアニメーションでナビを開閉機能実装 */
-//   console.log('hello');
-//   navi.getNavClose();
-//   navi.getNoneNavClose();
-//   // navi.getnavOpen();
-// });
-// loadScript('./../js/ps.js', function(){
-//   var id = 'PSection';
-//   var targetId = document.getElementById(id);
-//   var photoSection = new PhotosSection(targetId);
-//   var targetClass = 'photos-sect-open';
-//   photoSection.getPSOpen(targetClass);
-// });
 document.onreadystatechange = function() {
   if(document.readyState == "complete") {
     var hero = 'hero';
